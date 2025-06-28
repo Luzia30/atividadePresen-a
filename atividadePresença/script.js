206bda26d74365f2cf2b19330d044c8b2d5c4ece
Luzia30 <luzia.simao08@aluno.ifce.edu.br>
2025-06-28 15:19:55 -0300

    Update script.js

--- atividadePresença/script.js
+++ atividadePresença/script.js
@@ -1,4 +1,7 @@
+function generateRandomHexColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
 document.getElementById('colorChangeButton').addEventListener('click', function() {
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        document.body.style.backgroundColor = randomColor;
+    document.body.style.backgroundColor = generateRandomHexColor();
 });
